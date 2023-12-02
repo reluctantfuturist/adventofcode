8e4adfae9ef375a43f6ec20a77fa9adec5a5d4a9
reluctantfuturist <alexey.b.rybak@gmail.com>
2023-12-01 20:37:11 -0800

    minor fixes

--- 2015/src/day3/day3.spec.ts
+++ 2015/src/day3/day3.spec.ts
@@ -2,13 +2,29 @@ import day3 from "./index";
 
 describe("On Day 3", () => {
   describe("Part 1", () => {
-    it(`part1 is identity function`, () => {
-      expect(day3.solveForPartOne("hello")).toBe("hello");
+    it("delivers presents to 2 houses: one at the starting location, and one to the east", () => {
+      expect(day3.solveForPartOne(">")).toBe("2");
+    });
+
+    it("delivers presents to 4 houses in a square, including twice to the house at his starting/ending location", () => {
+      expect(day3.solveForPartOne("^>v<")).toBe("4");
+    });
+
+    it("delivers a bunch of presents to some very lucky children at only 2 houses", () => {
+      expect(day3.solveForPartOne("^v^v^v^v^v")).toBe("2");
     });
   });
   describe("Part 2", () => {
-    it(`part1 is identity function`, () => {
-      expect(day3.solveForPartOne("hello")).toBe("hello");
+    it("delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south", () => {
+      expect(day3.solveForPartTwo("^v")).toBe("3");
+    });
+
+    it("delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started", () => {
+      expect(day3.solveForPartTwo("^>v<")).toBe("3");
+    });
+
+    it("delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other", () => {
+      expect(day3.solveForPartTwo("^v^v^v^v^v")).toBe("11");
     });
   });
 });
--- 2015/src/day3/index.ts
+++ 2015/src/day3/index.ts
@@ -5,12 +5,94 @@ class Day3 extends Day {
     super(3, 2015);
   }
 
+  private countHousesWithPresents(directions: string): number {
+    const visitedHouses = new Set<string>();
+    let x = 0; // Horizontal position
+    let y = 0; // Vertical position
+
+    // Add the starting location
+    visitedHouses.add(`${x},${y}`);
+
+    for (const direction of directions) {
+      switch (direction) {
+        case "^":
+          y++;
+          break; // North
+        case "v":
+          y--;
+          break; // South
+        case ">":
+          x++;
+          break; // East
+        case "<":
+          x--;
+          break; // West
+      }
+      visitedHouses.add(`${x},${y}`);
+    }
+
+    return visitedHouses.size;
+  }
+
+  private countHousesWithTeam(directions: string): number {
+    const visitedHouses = new Set<string>();
+    let santaX = 0,
+      santaY = 0; // Santa's position
+    let roboX = 0,
+      roboY = 0; // Robo-Santa's position
+    let isSantaTurn = true; // Flag to alternate between Santa and Robo-Santa
+
+    // Add the starting location
+    visitedHouses.add(`0,0`);
+
+    for (const direction of directions) {
+      if (isSantaTurn) {
+        // Move Santa
+        switch (direction) {
+          case "^":
+            santaY++;
+            break; // North
+          case "v":
+            santaY--;
+            break; // South
+          case ">":
+            santaX++;
+            break; // East
+          case "<":
+            santaX--;
+            break; // West
+        }
+        visitedHouses.add(`${santaX},${santaY}`);
+      } else {
+        // Move Robo-Santa
+        switch (direction) {
+          case "^":
+            roboY++;
+            break; // North
+          case "v":
+            roboY--;
+            break; // South
+          case ">":
+            roboX++;
+            break; // East
+          case "<":
+            roboX--;
+            break; // West
+        }
+        visitedHouses.add(`${roboX},${roboY}`);
+      }
+      isSantaTurn = !isSantaTurn; // Alternate turns
+    }
+
+    return visitedHouses.size;
+  }
+
   solveForPartOne(input: string): string {
-    return input;
+    return this.countHousesWithPresents(input).toString();
   }
 
   solveForPartTwo(input: string): string {
-    return input;
+    return this.countHousesWithTeam(input).toString();
   }
 }
 
